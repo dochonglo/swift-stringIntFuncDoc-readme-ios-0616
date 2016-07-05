6ceb0d15ad2a3695da8a8790ac467eb5462624ec
dochonglo <ismaelbarry777@hotmail.com>
2016-07-05 18:31:08 -0400

    Swift Function Documentation Readme is Complete

--- Help.playground/Contents.swift
+++ Help.playground/Contents.swift
@@ -4,3 +4,7 @@ name.uppercaseString
 let uppercaseName = name.uppercaseString
 print("\(name)... \(name)... \(uppercaseName)! WAKE UP!")
 // prints "Audrey... Audrey... AUDREY! WAKE UP!")
+
+// Called a string method to lowercase the name.
+let lowercaseName = uppercaseName.lowercaseString
+print(lowercaseName)
